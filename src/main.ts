0241a1db12b3bc4f736e5f33f566cc2e26c396b8
iago-dev <joseisgolima@gmail.com>
2026-07-06 17:13:39 -0300

    refactor: agora e serveless

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
+import express from 'express';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let app: express.Express;
 
-  app.use(helmet());
-  app.enableCors({
+async function bootstrap() {
+  if (app) return app;
+
+  const expressApp = express();
+  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+  nestApp.use(helmet());
+  nestApp.enableCors({
     origin: (process.env.CORS_ORIGIN || 'http://localhost:5173').split(',').map((o) => o.trim().replace(/\/$/, '')),
     credentials: true,
   });
-
-  app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(
+  nestApp.setGlobalPrefix('api');
+  nestApp.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
@@ -29,12 +34,22 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+  const document = SwaggerModule.createDocument(nestApp, config);
+  SwaggerModule.setup('api/docs', nestApp, document);
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
-
-  const port = process.env.API_PORT || 3000;
-  await app.listen(port);
-  console.log(`API running on http://localhost:${port}`);
+  await nestApp.init();
+  app = expressApp;
+  return app;
 }
-bootstrap();
+
+export default async (req: express.Request, res: express.Response) => {
+  const expressApp = await bootstrap();
+  expressApp(req, res);
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.API_PORT || 3000;
+  bootstrap().then((expressApp) => {
+    expressApp.listen(port, () => console.log(`API running on http://localhost:${port}`));
+  });
+}
